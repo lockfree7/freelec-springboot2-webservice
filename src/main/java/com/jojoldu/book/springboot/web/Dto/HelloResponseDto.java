75b7c143bf3fd80365165bafc0e9c4730b5e3408
lockfree7 <lockfree7@gmail.com>
2021-11-06 12:46:32 +0900

    2장 예제까지 모두 성공!

--- src/main/java/com/jojoldu/book/springboot/web/Dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/Dto/HelloResponseDto.java
@@ -1,12 +1,14 @@
-package com.jojoldu.book.springboot.web.dto;
+package com.jojoldu.book.springboot.web.Dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+
 public class HelloResponseDto {
 
     private final String name;
     private final int amount;
+
 }
